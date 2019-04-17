a31203eb9c06f0f8ddfca669cb3dba91f176b884
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2019-04-17 10:21:32 +0600

    create scoll top

---
+++ src/assets/scripts/modules/fancy-scroll.js
@@ -0,0 +1,10 @@
+import $ from "jquery";
+$('[data-role="fancy-scroll"]').click(function() {
+    $("html, body").animate({
+       scrollTop: $($(this).attr("data-slide")).offset().top + "px"
+    }, {
+       duration: 800,
+       easing: "swing"
+    });
+    return false;
+ });
