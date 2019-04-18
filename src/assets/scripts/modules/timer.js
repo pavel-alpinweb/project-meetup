36feab40a9092bb355657d647a4c772d92e924cd
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2019-04-18 10:49:45 +0600

    upgrade counter

--- src/assets/scripts/modules/timer.js
+++ src/assets/scripts/modules/timer.js
@@ -1,39 +1,47 @@
-var target_date = new Date().getTime() + (1000*3600*24*60); // установить дату обратного отсчета
-var days, hours, minutes, seconds; // переменные для единиц времени
- 
-var daysView = document.getElementById("days");
-var hoursView = document.getElementById("hours");
-var minutesView = document.getElementById("minutes");
-var secondsView = document.getElementById("seconds");
+import $ from "jquery";
+$(function () {
+    var element = $('[data-role="timer"]');
+    element.click(function(){
+        console.log($(this).attr('data-final'));
+        var current_date = $(this).attr('data-final');
 
+        var target_date = new Date(current_date).getTime(); // установить дату обратного отсчета
+        var days, hours, minutes, seconds; // переменные для единиц времени
+        
+        var daysView = $(this).find('[data-days]');
+        var hoursView =  $(this).find('[data-hours]');
+        var minutesView = $(this).find('[data-minutes]');
+        var secondsView = $(this).find('[data-seconds]');
+        
+        getCountdown();
+        
+        setInterval(function () { getCountdown(); }, 1000);
+        
+        function getCountdown(){
+        
+            var current_date = new Date().getTime();
+            var seconds_left = (target_date - current_date) / 1000;
+        
+            days = pad( parseInt(seconds_left / 86400) );
+            seconds_left = seconds_left % 86400;
+                
+            hours = pad( parseInt(seconds_left / 3600) );
+            seconds_left = seconds_left % 3600;
+                
+            minutes = pad( parseInt(seconds_left / 60) );
+            seconds = pad( parseInt( seconds_left % 60 ) );
+    
+            daysView.html(days);
+            hoursView.html(hours);
+            minutesView.html(minutes);
+            secondsView.html(seconds);
+    
+        }
+        
+        function pad(n) {
+            return (n < 10 ? '0' : '') + n;
+        } 
+    });   
 
-var countdown = document.getElementById("tiles"); // получить элемент тега
- 
-getCountdown();
- 
-setInterval(function () { getCountdown(); }, 1000);
- 
-function getCountdown(){
- 
-    var current_date = new Date().getTime();
-    var seconds_left = (target_date - current_date) / 1000;
- 
-    days = pad( parseInt(seconds_left / 86400) );
-    seconds_left = seconds_left % 86400;
-          
-    hours = pad( parseInt(seconds_left / 3600) );
-    seconds_left = seconds_left % 3600;
-           
-    minutes = pad( parseInt(seconds_left / 60) );
-    seconds = pad( parseInt( seconds_left % 60 ) );
-
-    daysView.innerHTML = days;
-    hoursView.innerHTML = hours;
-    minutesView.innerHTML = minutes;
-    secondsView.innerHTML = seconds;
-
-}
- 
-function pad(n) {
-    return (n < 10 ? '0' : '') + n;
-}
+    $(element).click(); 
+});
