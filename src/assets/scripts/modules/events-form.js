635c50da076b8930388ad9bd1eb01dcdaa3885a7
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2019-05-15 20:51:27 +0600

    events & speakers form refactoring

--- src/assets/scripts/modules/events-form.js
+++ src/assets/scripts/modules/events-form.js
@@ -1,15 +1,15 @@
-var tableRow = $('[data-role="events-form-row"]:last-child');
+var tableRow = $('[data-role="events-form"] [data-role="form-row"]:last-child');
 
-$('[data-role="add-table-row"]').click(function (e) { 
+$('[data-role="add-table-row-events"]').click(function (e) { 
     e.preventDefault();
-    tableRow.clone().appendTo('[data-role="events-form-body"]');
-});
-
-$('[data-role="save-table"]').click(function (e) { 
-    e.preventDefault();
+    tableRow.clone().appendTo('[data-role="events-form"] [data-role="form-body"]');
 });
 
 $('html, body').on('click', '[data-role="remove-table-row"]', function (e) {
     e.preventDefault();
-    $(this).parents('[data-role="events-form-row"]').remove();
+    $(this).parents('[data-role="form-row"]').remove();
+});
+
+$('[data-role="save-table-events"]').click(function (e) { 
+    e.preventDefault();
 });
--- src/assets/scripts/modules/speakers-form.js
+++ src/assets/scripts/modules/speakers-form.js
@@ -1,15 +1,31 @@
-var tableRow = $('[data-role="speakers-form-row"]:last-child');
+var tableRow = $('[data-role="speakers-form"] [data-role="form-row"]:last-child');
 
-$('[data-role="add-table-row-speakers"]').click(function (e) { 
+$('body').on('click', '[data-role="remove-table-row-speakers"]', function (e) {
     e.preventDefault();
-    tableRow.clone().appendTo('[data-role="speakers-form-body"]');
+    $(this).parents('[data-role="speakers-form-row"]').remove();
+});
+
+$('body').on('click', '[data-role="soc-flag"]', function () {
+    if($(this).next().val() != ''){
+        $(this).parent().toggleClass('is-active');
+    }
+});
+
+$('body').on('click', '[data-role="social-input"]', function () {
+    $(this).parent().addClass('is-active');
+});
+
+$('body').on('focusout', '[data-role="social-input"]', function () {
+    if($(this).val() == ''){
+        $(this).parent().removeClass('is-active');
+    }
 });
 
 $('[data-role="save-table-speakers"]').click(function (e) { 
     e.preventDefault();
 });
 
-$('html, body').on('click', '[data-role="remove-table-row-speakers"]', function (e) {
+$('[data-role="add-table-row-speakers"]').click(function (e) { 
     e.preventDefault();
-    $(this).parents('[data-role="speakers-form-row"]').remove();
+    tableRow.clone().appendTo('[data-role="speakers-form"]  [data-role="form-body"]');
 });
