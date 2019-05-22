8525982756ea5c1af51aec104c4f73810a76ec2f
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2019-05-22 14:42:01 +0600

    create events-table-form request

--- src/assets/scripts/modules/events-form.js
+++ src/assets/scripts/modules/events-form.js
@@ -1,4 +1,7 @@
+import sendData from './adminAjax';
+
 var tableRow = $('[data-role="events-form"] [data-role="form-row"]:last-child');
+var eventsArray = [];
 
 $('[data-role="add-table-row-events"]').click(function (e) { 
     e.preventDefault();
@@ -12,4 +15,22 @@ $('html, body').on('click', '[data-role="remove-table-row"]', function (e) {
 
 $('[data-role="save-table-events"]').click(function (e) { 
     e.preventDefault();
+    eventsArray = [];
+    var eventsRows = $('[data-role="events-form"] [data-role="form-row"]'); 
+    eventsRows.each(function(){
+        var eventsObject = {};
+        eventsObject.time = $(this).find('[data-input="event-time"]').val();
+        eventsObject.name = $(this).find('[data-input="event-name"]').val();
+        eventsArray.push(eventsObject);
+    });
+    console.log(eventsArray);
+
+    if(eventsRows.length == 0){
+        $('[data-role="server message"]').removeClass('hide').addClass('m-fail').text('Необходимо добавить хотябы одно событие!');
+        var hideMessage = setTimeout(function(){
+            $('[data-role="server message"]').addClass('hide');
+        }, 3000);
+    } else {
+        sendData("/saveWelcomeText", eventsArray, "Расписание событий обновленно!");
+    } 
 });
