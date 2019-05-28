14b36520a745c55eaa443f29f2259f6f8f9565c5
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2019-05-28 15:06:06 +0600

    replace data from vars.pug to database

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,22 @@
+const db = require("../models/db");
+const header = db.getState().header;
+const mainBackgorund = db.getState().mainBackgorund;
+const welcomeText = db.getState().welcomeText;
+const promoText = db.getState().promoText;
+const gallery = db.getState().gallery;
+const events = db.getState().events;
+const contacts = db.getState().contacts;
+const speakers = db.getState().speakers;
+
 module.exports.get = function(req, res) {
-  res.render("pages/index");
+  res.render("pages/index", {
+    header: header,
+    mainBackgorund: mainBackgorund,
+    welcomeText: welcomeText,
+    promoText: promoText,
+    gallery: gallery,
+    events: events,
+    contacts: contacts,
+    speakers: speakers
+  });
 };
