9d9a7fc11d7874b23943b74df19fc23fa6523a20
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2019-06-03 14:00:58 +0600

    fix gallery

--- src/assets/scripts/modules/gallery.js
+++ src/assets/scripts/modules/gallery.js
@@ -1,6 +1,9 @@
 import Masonry from 'masonry-layout';
-var elem = document.querySelector('.gallery');
-var msnry = new Masonry( elem, {
-  // options
-  itemSelector: '.gallery__item',
+
+$(document).ready(function () {
+  var elem = document.querySelector('.gallery');
+  var msnry = new Masonry( elem, {
+    // options
+    itemSelector: '.gallery__item',
+  });
 });
