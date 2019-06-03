27e51637addfac5e54ae681d1330468e71290b86
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2019-06-03 14:39:33 +0600

    build gallery layout after loading images

--- src/assets/scripts/modules/gallery.js
+++ src/assets/scripts/modules/gallery.js
@@ -1,9 +1,12 @@
 import Masonry from 'masonry-layout';
 
 $(document).ready(function () {
-  var elem = document.querySelector('.gallery');
-  var msnry = new Masonry( elem, {
-    // options
-    itemSelector: '.gallery__item',
+  $('.gallery').imagesLoaded( function() {
+    var elem = document.querySelector('.gallery');
+    var msnry = new Masonry( elem, {
+      // options
+      itemSelector: '.gallery__item',
+    });
+    msnry.reloadItems();
   });
 });
